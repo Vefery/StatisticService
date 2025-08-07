6dc867c43db3cb2eb30cab29666c23ddc39ff08a
Vefery <54257165+Vefery@users.noreply.github.com>
2025-08-07 16:07:54 +0700

    Added DTO and entity conversions

--- DTOs/StatisticEntryDTO.cs
+++ DTOs/StatisticEntryDTO.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Statistic.Entities;
 
 namespace Statistic.DTOs
 {
@@ -8,5 +9,23 @@ namespace Statistic.DTOs
         [Required] DateTime startTime,
         [Required] DateTime endTime,
         [Required] string version
-    );
+    )
+    {
+        public StatisticEntry ToEntiry()
+        {
+            return new StatisticEntry
+            {
+                DeviceId = _id,
+                Name = name,
+                StartTime = startTime,
+                EndTime = endTime,
+                Version = version
+            };
+        }
+
+        public static StatisticEntryDTO ToDTO(StatisticEntry entity)
+        {
+            return new StatisticEntryDTO(entity.DeviceId, entity.Name, entity.StartTime, entity.EndTime, entity.Version);
+        }
+    }
 }

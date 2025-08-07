303c358d36a36fff2aff11741c85bcaeb45819e4
Vefery <54257165+Vefery@users.noreply.github.com>
2025-08-07 14:17:56 +0700

    Initial commit

---
+++ Endpoints/StatisticsEndpoints.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Statistic.Endpoints
+{
+    public static class StatisticsEndpoints
+    {
+        public static RouteGroupBuilder MapStatisticEndpoints(this WebApplication app)
+        {
+            RouteGroupBuilder group = app.MapGroup("statistics");
+
+            group.MapGet("/", () =>
+            {
+                return Results.NoContent;
+            });
+
+            return group;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+namespace Statistic
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var app = builder.Build();
+
+            app.Run();
+        }
+    }
+}
